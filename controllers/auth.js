68e4d62d080116b72ec48a77704eeb7bf977f6b6
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-01 18:17:29 +0400

    converted user password to the hash code to be more secure, and also created tokens with json web token package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,12 @@
 const User = require("../models/user");
 const { StatusCodes } = require("http-status-codes");
 
-const register = (req, res) => {
-  res.status(StatusCodes.CREATED).send(req.body);
+const register = async (req, res) => {
+  const user = await User.create({ ...req.body });
+  const token = user.createJWT();
+  res
+    .status(StatusCodes.CREATED)
+    .send({ user: { username: user.name }, token });
 };
 
 const login = (req, res) => {
