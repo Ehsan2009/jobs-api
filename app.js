5863e0197636d3ef179a382c31526c88bb6e2c62
Ehsan Javdan <ehsanjavdan77@gmail.com>
2026-05-08 22:37:01 +0400

    fixed some bugs and also commented xss-clean package usage, becuase it seemms like it is not compatibale with my express version

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 // extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
-const xss = require("xss-clean");
+// const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
 const app = express();
@@ -31,7 +31,7 @@ app.use(
 );
 app.use(express.json());
 app.use(cors());
-app.use(xss());
+// app.use(xss());
 
 app.get("/", (req, res) => {
   res.send("jobs api");
